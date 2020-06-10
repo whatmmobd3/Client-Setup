8b76f34c6541dbf807692b3bb4ce6173b193dc5e
quangloc.nguyen <quangloc.nguyen>
2020-06-10 11:08:54 +0700

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Header from "./Header";
 
-const App = () => {
+const App = ({ children }) => {
   return (
     <div>
       <Header />
-      App
+      {children}
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 
+import Welcome from "./components/Welcome";
 import App from "./components/App";
+
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <App>
+      <Route extract path="/" component={Welcome} />
+    </App>
   </BrowserRouter>,
   document.querySelector("#root")
 );
