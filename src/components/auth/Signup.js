9d009c78e66c883442d45a8a62b55b84a9ec7026
quangloc.nguyen <quangloc.nguyen>
2020-06-10 13:28:21 +0700

    scaffolding

---
+++ src/components/auth/Signup.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <form>
+        <fieldset>
+          <label>Email</label>
+        </fieldset>
+        <fieldset>
+          <label>Password</label>
+        </fieldset>
+      </form>
+    );
+  }
+}
+
+export default App
