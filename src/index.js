7e055171a9ecf9961984462fd4f3c0eaa9e94edc
quangloc.nguyen <quangloc.nguyen>
2020-06-10 13:52:37 +0700

    install redux

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route } from "react-router-dom";
+import {Provider} from "react-redux"
+import {createStore} from "redux"
+import reducers from './components/reducers'
 
 import Welcome from "./components/Welcome";
 import App from "./components/App";
 import Signup from './components/auth/Signup';
 
 ReactDOM.render(
+  <Provider store={createStore(reducers,{})}>
+
   <BrowserRouter>
     <App>
       <Route extract path="/" component={Welcome} />
+      <Route  path="/signup" component={Signup} />
     </App>
-  </BrowserRouter>,
+  </BrowserRouter>
+  </Provider>,
   document.querySelector("#root")
 );
