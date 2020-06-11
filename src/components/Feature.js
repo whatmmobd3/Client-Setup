33e88a74139b4d06c5f65b64cc417cc73f7bbb93
quangloc.nguyen <quangloc.nguyen>
2020-06-11 13:53:48 +0700

    feature

---
+++ src/components/Feature.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return <h2>This is Feature</h2>
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import reduxThunk from "redux-thunk";
 
-import reducers from './reducers'
+import reducers from "./reducers";
 import Welcome from "./components/Welcome";
 import App from "./components/App";
 import Signup from "./components/auth/Signup";
+import Feature from './components/Feature'
+
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
@@ -16,8 +18,9 @@ ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <App>
-        <Route extract path="/" component={Welcome} />
+        <Route path="/" exact component={Welcome} />
         <Route path="/signup" component={Signup} />
+        <Route path="/feature" component={Feature} />
       </App>
     </BrowserRouter>
   </Provider>,
